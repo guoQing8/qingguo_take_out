4a9b9fcf7f7436016aaf34cc572e71c4a2636880
superlistudy <2500016802@qq.com>
2022-05-07 15:18:52 +0800

    v1.0缓存开发

---
+++ src/main/java/com/superli/qingguo/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.superli.qingguo.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author superli
+ * @Description
+ * @Date 2022/5/7 15:15
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object>
+    redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
